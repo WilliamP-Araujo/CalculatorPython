fb10e348a417ff9e0d5ee06d48a8fcd49f28f57d
William P. Araújo <williampontearaujo@gmail.com>
2023-11-14 22:41:56 -0300

    first commit

---
+++ PycharmProjects/pythonCalculator/Calc.py
@@ -0,0 +1,7 @@
+num1 = input("Insira seu primeiro numero:")
+num2 = input("Insira seu primeiro numero:")
+
+resultado = float(num1) + float(num2)
+
+print(resultado)
+print("Muito obrigado por utilizar meu software!")
